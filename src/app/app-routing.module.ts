feeaf228ea090438249fb42fdf970b8fedc3db9c
gustavokida <gustavokida@live.com>
2023-08-04 00:43:17 -0400

    refactor: mudança no nome da rota para home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { UrlFormComponent } from './url/url-form/url-form.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'enviar', pathMatch: 'full'},
-  {path: 'enviar', component: UrlFormComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: UrlFormComponent},
 ];
 
 @NgModule({
